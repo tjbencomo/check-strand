5dfd075ac2a1dd89148268df2892af6ad819ff11
Tomas Bencomo <tjbencomo@gmail.com>
2020-12-11 13:51:13 -0800

    Add CLI args and ability to run without python command

--- check-strand.py
+++ check-strand.py
@@ -1,11 +1,29 @@
+#!/usr/bin/env python
+
 import os
 import sys
 import tempfile
-import subprocess as sb
 import gzip
+import argparse
+import subprocess as sb
 import pandas as pd
 
-def get_reads(inputfp, outfp, num_reads=5e4):
+DEFAULT_NREADS = 5e4
+DEFAULT_THREADS = 2
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Check RNA-Seq stranding')
+    parser.add_argument('--index', '-i', required=True, type=str, nargs=1, help='kallisto index file', 
+                        dest='index')
+    parser.add_argument('--num-reads', '-n', type=int, nargs=1, help='number of reads to check (default: 5e4)',
+                        dest='num-reads')
+    parser.add_argument('--threads', '-t', type=int, nargs=1, help='number of threads for kallisto (default: 2)',
+                        dest='threads')
+    parser.add_argument('samples', type=str, nargs=1, help='CSV file with fqs to check')
+    args = parser.parse_args()
+    return args
+
+def get_reads(inputfp, outfp, num_reads):
     if inputfp.endswith('gz'):
         with gzip.open(inputfp, 'rb') as f:
             lines = [next(f).decode("utf-8") for i in range(int(num_reads*4))]
@@ -24,20 +42,20 @@ def strand_scoring(uncts, rfcts, frcts):
     else:
         return 'unstranded'
 
-def infer_strand(fq1, fq2, idx_fp):
+def infer_strand(fq1, fq2, idx_fp, nreads=5e4, threads=2):
     print(f"Inferring strand from {fq1}, {fq2}")
     with tempfile.TemporaryDirectory() as tmpdirname:
         fq1_fp = os.path.join(tmpdirname, 'test_1.fq')
         fq2_fp = os.path.join(tmpdirname, 'test_2.fq')
 
-        get_reads(fq1, fq1_fp)
-        get_reads(fq2, fq2_fp)
+        get_reads(fq1, fq1_fp, nreads)
+        get_reads(fq2, fq2_fp, nreads)
 
-        sb.run(['kallisto', 'quant', '-i', idx_fp, '-t', '2','-o', os.path.join(tmpdirname, 'un'), 
+        sb.run(['kallisto', 'quant', '-i', idx_fp, '-t', str(threads),'-o', os.path.join(tmpdirname, 'un'), 
                 fq1_fp, fq2_fp], stdout=sb.DEVNULL, stderr=sb.DEVNULL)
-        sb.run(['kallisto', 'quant', '-i', idx_fp, '-t', '2','-o', os.path.join(tmpdirname, 'rf'), 
+        sb.run(['kallisto', 'quant', '-i', idx_fp, '-t', str(threads),'-o', os.path.join(tmpdirname, 'rf'), 
                 '--rf-stranded', fq1_fp, fq2_fp], stdout=sb.DEVNULL, stderr=sb.DEVNULL)
-        sb.run(['kallisto', 'quant', '-i', idx_fp, '-t', '2','-o', os.path.join(tmpdirname, 'fr'), 
+        sb.run(['kallisto', 'quant', '-i', idx_fp, '-t', str(threads),'-o', os.path.join(tmpdirname, 'fr'), 
                 '--fr-stranded', fq1_fp, fq2_fp], stdout=sb.DEVNULL, stderr=sb.DEVNULL)
 
         res_fps = [os.path.join(tmpdirname, d, 'abundance.tsv') for d in ['un', 'rf', 'fr']]
@@ -48,12 +66,23 @@ def infer_strand(fq1, fq2, idx_fp):
         return strand
 
 def main():
-    idx_fp = sys.argv[1]
-    samples_fp = sys.argv[2]
+    args = get_args()
+    idx_fp = args['index']
+    samples_fp = args['samples']
+    
     samples = pd.read_csv(samples_fp)
+    
+    num_reads = args['num-reads']
+    num_threads = args['threads']
+    
+    if num_reads is None:
+        num_reads = DEFAULT_NREADS
+    if num_threads is None:
+        num_threads = DEFAULT_THREADS
+
     res = []
     for index, row in samples.iterrows():
-        res.append(infer_strand(row['fq1'], row['fq2'], idx_fp))
+        res.append(infer_strand(row['fq1'], row['fq2'], idx_fp, num_reads, num_threads))
     samples['strand'] = res
     fn = samples_fp[0:samples_fp.find('.csv')]
     outfn = f"{fn}.stranded.csv"
